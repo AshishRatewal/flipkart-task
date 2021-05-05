84a81e850e7afd13ed1381cc6d4eef6d56b1868b
AshishRatewal <mr.ashishparjapat@gmail.com>
2021-05-06 01:14:21 +0530

    the login id password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 const Login = () => {
+  const loginId = "admin@gmail.com";
+  const loginPass = "123456789";
+  const inputObject = {
+    email: "",
+    password: "",
+  };
+  const [inputs, setInputs] = useState(inputObject);
+  const [err, setErr] = useState(inputObject);
+  const getLoginData = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    checkErr();
+  };
+
+  const validator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const checkErr = () => {
+    let emptyErr = {};
+    const { email, password } = inputs;
+    if (email == "" || email == null) {
+      emptyErr.emailErr = "***Please Enter an valid Email";
+    } else if (!validator.test(email)) {
+      emptyErr.emailErr = "***Please Enter Email";
+    } else {
+      emptyErr.emailErr = "";
+    }
+
+    // password
+    if (password == "" || password == null) {
+      emptyErr.passwordErr = "***Please Enter Password";
+    } else if (password.length < 8) {
+      emptyErr.passwordErr = "***Password must be of 8 character";
+    } else {
+      emptyErr.passwordErr = "";
+    }
+
+    setErr(emptyErr);
+  };
+
+  // the final login data
+  let flipLogin = (e) => {
+    e.preventDefault();
+    const { email, password } = inputs;
+    if (
+      email == "" ||
+      email == null ||
+      password == "" ||
+      !password.length >= 8 ||
+      !validator.test(email)
+    ) {
+      checkErr();
+    } else {
+      if(email == loginId && password == loginPass){
+        window.alert("data matched");
+      }else{
+        window.alert("Sorry! Data not matched");
+      }
+    }
+  };
   return (
     <>
       <div className="container py-5">
@@ -10,25 +71,39 @@ const Login = () => {
           <div className="col-12">
             <form>
               <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
+                <label htmlFor="email">Email address</label>
                 <input
                   type="email"
                   className="form-control"
-                  id="exampleInputEmail1"
+                  id="email"
+                  name="email"
                   aria-describedby="emailHelp"
                   placeholder="Enter email"
+                  onChange={getLoginData}
+                  autoComplete="off"
+                  // value={inputObject.email}
                 />
+                <small className="text-danger">{err.emailErr}</small>
               </div>
               <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
+                <label htmlFor="password">Password</label>
                 <input
                   type="password"
                   className="form-control"
-                  id="exampleInputPassword1"
+                  id="password"
+                  name="password"
                   placeholder="Password"
+                  onChange={getLoginData}
+                  autoComplete="off"
+                  // value={inputObject.password}
                 />
+                <small className="text-danger">{err.passwordErr}</small>
               </div>
-              <button type="submit" className="btn btn-primary btn-block">
+              <button
+                type="submit"
+                className="btn btn-primary btn-block"
+                onClick={flipLogin}
+              >
                 Submit
               </button>
             </form>
